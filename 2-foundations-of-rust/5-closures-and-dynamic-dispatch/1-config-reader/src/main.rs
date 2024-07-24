437a597c639abb333dcb1e86c4a35b055c670ba4
Yao Fu <fubupc@gmail.com>
2024-07-24 11:25:19 +0800

    2-foundations-of-rust/5-closures-and-dynamic-dispatch/1-config-reader

--- 2-foundations-of-rust/5-closures-and-dynamic-dispatch/1-config-reader/src/main.rs
+++ 2-foundations-of-rust/5-closures-and-dynamic-dispatch/1-config-reader/src/main.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 /// An imaginary config file
 #[derive(Serialize, Deserialize, Debug)]
@@ -25,7 +25,19 @@ trait DeserializeConfig {
     fn deserialize<'a>(&self, contents: &'a str) -> Result<Config<'a>, Error>;
 }
 
-// TODO add some types that implement `DeserializeConfig`
+struct Json;
+impl DeserializeConfig for Json {
+    fn deserialize<'a>(&self, contents: &'a str) -> Result<Config<'a>, Error> {
+        serde_json::from_str(contents).map_err(|e| Error::Json(e))
+    }
+}
+
+struct Yaml;
+impl DeserializeConfig for Yaml {
+    fn deserialize<'a>(&self, contents: &'a str) -> Result<Config<'a>, Error> {
+        serde_yaml::from_str(contents).map_err(|e| Error::Yaml(e))
+    }
+}
 
 fn main() {
     let mut args = std::env::args();
@@ -40,12 +52,35 @@ fn main() {
         Ok(c) => c,
         Err(e) => {
             // `path` was created from an UTF-8 string, so can be converted to one
-            eprintln!("Error reading file at path {}: {}", path.to_str().unwrap(), e);
+            eprintln!(
+                "Error reading file at path {}: {}",
+                path.to_str().unwrap(),
+                e
+            );
             return;
         }
     };
 
-    let config: Config = todo!("Deserialize `file_contents` using either serde_yaml or serde_json depending on the file extension. Use dynamic dispatch");
+    let deserializer: &dyn DeserializeConfig = match _extension {
+        Some("json") => &Json,
+        Some("yml") => &Yaml,
+        Some(ext) => {
+            eprintln!("Unkown file type: {}", ext);
+            return;
+        }
+        None => {
+            eprintln!("No extension");
+            return;
+        }
+    };
+
+    let config: Config = match deserializer.deserialize(&file_contents) {
+        Ok(config) => config,
+        Err(err) => {
+            eprintln!("Deserialize config failed: {:?}", err);
+            return;
+        }
+    };
 
     println!("Config was: {config:?}");
 }
