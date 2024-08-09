9b5c7e2b7f4e9550c61a7ca58fdea171024f2e38
Yao Fu <fubupc@gmail.com>
2024-08-09 16:18:22 +0800

    4-multitasking/2-parallel-multitasking/1-tf-idf

--- 4-multitasking/2-parallel-multitasking/1-tf-idf/src/main.rs
+++ 4-multitasking/2-parallel-multitasking/1-tf-idf/src/main.rs
@@ -29,7 +29,13 @@ fn term_frequency(document: &str) -> HashMap<&str, usize> {
         // good enough definition of "word" for this exercise
         .split_whitespace()
         // using fold to get some extra practice with monoids. Using a for loop is also totally fine.
-        .fold(HashMap::default(), |mut hash_map, word| todo!())
+        .fold(HashMap::default(), |mut hash_map, word| {
+            hash_map
+                .entry(word)
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
+            hash_map
+        })
 }
 
 fn combine_occurrences<'a>(
@@ -41,12 +47,23 @@ fn combine_occurrences<'a>(
     //
     // NOTE: we're already using all of our cores to process whole documents. Using a parallel iterator
     // here would likely make performance worse!
-    b.into_iter().fold(a, |mut hash_map, (word, count)| todo!())
+    b.into_iter().fold(a, |mut hash_map, (word, count)| {
+        hash_map
+            .entry(word)
+            .and_modify(|old| *old += count)
+            .or_insert(count);
+        hash_map
+    })
 }
 
 /// Map each word in the document to the value 1
 fn term_occurrence(document: &str) -> HashMap<&str, usize> {
-    todo!()
+    document
+        .split_whitespace()
+        .fold(HashMap::new(), |mut accum, word| {
+            accum.entry(word).or_insert(1);
+            accum
+        })
 }
 
 /// For each word, in how many of the documents it occurs
@@ -55,7 +72,9 @@ fn document_frequency<'a>(
 ) -> HashMap<&'a str, usize> {
     // map each document to a hashmap that maps words to whether they occur (use `term_occurrence`),
     // then reduce, combining the counts.
-    todo!()
+    documents
+        .map(|doc| term_occurrence(doc))
+        .reduce(|| HashMap::new(), |a, b| combine_occurrences(a, b))
 }
 
 fn score_document(
