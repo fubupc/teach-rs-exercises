3013f7981447c8c5b209774eb5a5c484f42478f1
Yao Fu <fubupc@gmail.com>
2024-08-27 00:06:49 +0800

    6-rust-for-systems-programming/1-foreign-function-interface/3-tweetnacl-bindgen

--- 6-rust-for-systems-programming/1-foreign-function-interface/3-tweetnacl-bindgen/src/main.rs
+++ 6-rust-for-systems-programming/1-foreign-function-interface/3-tweetnacl-bindgen/src/main.rs
@@ -1,3 +1,22 @@
+use std::mem::MaybeUninit;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", crypto_hash_sha512_tweet("Hello, world!".as_bytes()));
+}
+
+pub fn crypto_hash_sha512_tweet(data: &[u8]) -> [u8; 64] {
+    let mut ret: MaybeUninit<[u8; 64]> = MaybeUninit::uninit();
+    unsafe {
+        tweetnacl_bindgen::bindings::crypto_hash_sha512_tweet(
+            ret.as_mut_ptr() as *mut u8,
+            data.as_ptr(),
+            data.len() as _,
+        );
+        ret.assume_init()
+    }
+}
+
+pub fn crypto_hash_sha256_tweet(_data: &[u8]) -> [u8; 64] {
+    // TODO: crypto_hash_sha256_tweet not found in tweetnacl.c.
+    todo!()
 }
